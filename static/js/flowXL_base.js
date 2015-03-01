bdf5e6d38dd9dab23e13af398f76e295a77c8d22
crertel <crertel@talkbox>
2015-03-01 16:10:57 -0600

    Cleanup javascript.
    This also refactors some of the DOM manipulation into standalone(ish) functions.

--- static/js/flowXL_base.js
+++ static/js/flowXL_base.js
@@ -4,15 +4,12 @@ var flowXL = flowXL || {};
 flowXL.MarkerTable = function(){
 	this.details = "undefined";	// Will hold the details structure from jobs/1
 	this.$table = document.createElement("table");
-	//this.showButtons = true; // boolean; if the buttons should be rendered
-
 
 	/* Grabs the details for a job and places them in the details structure
 	 */
 	this.loadJob = loadJob;
 	function loadJob (jobID){
 		self = this;
-		//flowXL.MarkerTable.call(this);
 		var r = new XMLHttpRequest();
 		r.open("GET", "/api/1/jobs/" + jobID, false);
 		r.onreadystatechange = function () {
@@ -36,11 +33,8 @@ flowXL.MarkerTable = function(){
 		 * showButtons - (boolean) determines if the buttons should be shown
 		 *
 		 */
-
 		var $table = this.$table;
 		var details = this.details;
-
-		var this_mt = this;	
 		
 		// Nuke the current structure
 		while ($table.firstChild){
@@ -56,15 +50,8 @@ flowXL.MarkerTable = function(){
 		});
 		$table.appendChild($header);
 
-
-		// Now render each column
-		details.columns.forEach( function _addMarkerRows( marker ) {
-			//self = this;
-			var $row = document.createElement("tr");
-			// First column of the table
+        var buildButtonsForMarker = function( $row, marker ) {
 			var $button = document.createElement("td");
-			// This is empty in this implementation, but buttons for selection show up in this row
-			//$rowName.innerHTML = """<em>"+marker.name + "</em>";
 
 			if (showButtons){
 				// Now we check which kind of button we have 
@@ -77,199 +64,79 @@ flowXL.MarkerTable = function(){
 						buttonType = 'plus';
 					}
 				}
-				// Only show plus/minus button on those rows where all files have that marker
-				for (var i = 0; i < details.files.length; i++){
-					if (_.indexOf(marker.files, details.files[i]) == -1){
-						buttonType = '';
-					}
-				}	
 
-				if (_.indexOf(['plus', 'minus'], buttonType) != -1){
-					$button.innerHTML = "<button type='button' class='btn btn-default btn-sm' " + 
-						"id='" + marker.name + "'" +
-						">"+
-						"<span class='glyphicon glyphicon-" + buttonType + "'></span>" +
-						"</button>";
-					$button.addEventListener("click", function (){
-						var update = {};
-						if (buttonType == 'plus'){
-							update[marker.name] = true;
-						}
-						else{
-							update[marker.name] = false;
-						}
-						this_mt.toggleMarker(update);
-						this_mt.render(true)
-						console.log("clicked " + marker.name);
-					});
-				}
-				else{
-					$button.innerHTML ="<button type='button' class='btn btn-default btn-sm' disabled='true'>"+
-						"<span class='glyphicon glyphicon-warning-sign'></span>" +
-						"</button>";
-				}
-			}
-			else{
-				$button.innerHTML = "";
+				// Only show plus/minus button on those rows where all files have that marker
+                details.files.forEach( function( file ) {
+                    if ( !_.includes(marker.files, file) ) {
+                        buttonType = '';
+                    }
+                });
+
+                switch(buttonType) {
+                    case "plus": /* fall through */
+                    case "minus" : 
+                                    $button.innerHTML = "<button type='button' class='btn btn-default btn-sm' " + 
+                                        "id='" + marker.name + "'" +
+                                        ">"+
+                                        "<span class='glyphicon glyphicon-" + buttonType + "'></span>" +
+                                        "</button>";
+                                    $button.addEventListener("click", (function (){
+                                        var update = {};
+                                        update[marker.name] = buttonType =="plus";
+                                        this.toggleMarker(update);
+                                        this.render(true);
+                                        console.log("clicked " + marker.name);
+                                    }).bind(this));
+                                    break;
+                    default: $button.innerHTML = "<button type='button' class='btn btn-default btn-sm' disabled='true'>"+
+                        						 "<span class='glyphicon glyphicon-warning-sign'></span>" +
+						                          "</button>";
+                }
 			}
 			$row.appendChild($button);
+        };
 
-			// Build out sequential rows in the table
-			for (var i = 0; i < details.files.length; i++){
+        // function that creates a row for a marker
+        var renderMarkerRow = function ( marker ) {
+			var $row = document.createElement("tr");
+
+            buildButtonsForMarker.bind(this)( $row, marker);
+
+            // First column of the table
+            var addColumnsToRow = function ( $row, file ) {
 				var $rowEl = document.createElement("td");
-				var elClass = ""
+				var elClass = "";
 				$rowEl.innerHTML = marker.name;
 				
 				// See if the file has the marker labeled
-				if (_.indexOf(marker.files, details.files[i]) != -1){
-					elClass += "has-marker";
-				}
-				else {
-					elClass += "not-has-marker";
-				}
+                elClass += (_.includes(marker.files,file))? "has-marker ":"not-has-marker ";
+
 				// Next, see if the marker is active or not
-				elClass +=" ";
-				if (_.has(details.active, marker.name)){
-					if (details.active[marker.name]){
-						elClass += "active";
-					}
-					else{
-						elClass += "inactive";
-					}
-				}
-				else{
-					elClass += "inactive";
-				}
+                elClass += (details.active[marker.name])?"active":"inactive";
 
 				$rowEl.setAttribute("class", elClass);
 				$row.appendChild($rowEl);
-			}
+            };
+
+			// Build out sequential rows in the table
+            details.files.forEach( addColumnsToRow.bind(this, $row) );
+
 			// add the row to the table
 			$table.appendChild($row);
-		}); // End the loop over the table
+        };
 
-	return $table;
+		// Now render each column
+		details.columns.forEach( renderMarkerRow.bind(this) );
+                
+    	return $table;
 	}
 
 	this.toggleMarker = toggleMarker;
 	
 	function toggleMarker(newActive){
-		var this_mt = this;	
 		// Takes a dictionary of marker and updates the details.active appropreately
-		for (var key in newActive) {
-			if (this_mt.details.active.hasOwnProperty(key)){
-				this_mt.details.active[key] = newActive[key];
-			}
-			else{
-				console.log("Tried to change state of invalid key" + key);
-			}
-		}
+        _.merge( this.details.active, newActive);
 	};
 
 };
-
-
-
-flowXL.markerTable = ( function(details, showButtons) {
-	/* Takes a details object (following jobs/1 API) and a boolean
-	 * of wether or not to show buttons with this rendering
-	 * Returns a table element
-	 */
-	var $table = document.createElement("table");
-   
-	// if you have a problem with many, many files, it's probably because of this.
-	//$table.width ="100%";
-	
-	var $header = document.createElement("tr");
-	
-	["File Name:"].concat(details.files).forEach( function _addHeader( headerName ) {
-		var $columnHeader = document.createElement("th");
-		$columnHeader.innerHTML = "<strong>"+headerName+"</strong>";
-		$header.appendChild($columnHeader);
-	});
-	$table.appendChild($header);
-
-	/*
-	 * Start here to render columns!
-	 */
-	details.columns.forEach( function _addMarkerRows( marker ) {
-		var $row = document.createElement("tr");
-		
-		// First row of the table
-		var $button = document.createElement("td");
-		// This is empty in this implementation, but buttons for selection show up in this row
-		//$rowName.innerHTML = """<em>"+marker.name + "</em>";
-
-		if (showButtons){
-			// Now we check which kind of button we have 
-			var buttonType = "";
-			if (_.has(details.active, marker.name)){
-				if (details.active[marker.name]){
-					buttonType = 'minus';
-				}
-				else{
-					buttonType = 'plus';
-				}
-			}
-			// Only show plus/minus button on those rows where all files have that marker
-			for (var i = 0; i < details.files.length; i++){
-				if (_.indexOf(marker.files, details.files[i]) == -1){
-					buttonType = '';
-				}
-			}	
-
-			if (_.indexOf(['plus', 'minus'], buttonType) != -1){
-				$button.innerHTML = "<button type='button' class='btn btn-default btn-sm'>"+
-					"<span class='glyphicon glyphicon-" + buttonType + "'></span>" +
-					"</button>";
-			}
-			else{
-				$button.innerHTML ="<button type='button' class='btn btn-default btn-sm' disabled='true'>"+
-					"<span class='glyphicon glyphicon-warning-sign'></span>" +
-					"</button>";
-			}
-		}
-		else{
-			$button.innerHTML = "";
-		}
-		$row.appendChild($button);
-
-		// Build out sequential rows in the table
-		for (var i = 0; i < details.files.length; i++){
-			var $rowEl = document.createElement("td");
-			var elClass = ""
-			$rowEl.innerHTML = marker.name;
-			
-			// See if the file has the marker labeled
-			if (_.indexOf(marker.files, details.files[i]) != -1){
-				elClass += "has-marker";
-			}
-			else {
-				elClass += "not-has-marker";
-			}
-			// Next, see if the marker is active or not
-			elClass +=" ";
-			if (_.has(details.active, marker.name)){
-				if (details.active[marker.name]){
-					elClass += "active";
-				}
-				else{
-					elClass += "inactive";
-				}
-			}
-			else{
-				elClass += "inactive";
-			}
-
-			$rowEl.setAttribute("class", elClass);
-			$row.appendChild($rowEl);
-		}
-
-
-		// add the row to the table
-		$table.appendChild($row);
-	});
-	return $table;
-});
-
 
