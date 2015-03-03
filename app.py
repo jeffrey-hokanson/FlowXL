ae3969075add10282364f2179e11cf9c3e7bf41f
Jeffrey Hokanson (loki) <jeffrey@hokanson.us>
2015-03-02 18:19:59 -0600

    More work on updating for fileupload widget

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask, jsonify, render_template, json, request
+from flask import Flask, jsonify, render_template, json, request, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug import secure_filename
 import os
+from os.path import join
+import simplejson
 
+from lib.upload_file import uploadfile
 
 app = Flask(__name__)
 
+ALLOWED_EXTENSIONS = set(['fcs'])
+IGNORED_FILES = set(['.gitignore'])
 
 
 # Setup of the database
-dbfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test.db')
+dbfile = join(os.path.dirname(os.path.realpath(__file__)), 'test.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + dbfile
 db = SQLAlchemy(app)
+
+# Generic setup
+app.config['DATA_PATH'] = join(os.path.dirname(os.path.realpath(__file__)), 'data')
+
 
 class Active(db.Model):
 	__tablename__ = 'active'
@@ -32,7 +42,6 @@ class Active(db.Model):
 #	__tablename__ = 'marker'
 
 
-
 @app.route("/")
 def hello():
 	return render_template('main.html')
@@ -43,6 +52,80 @@ def make_col_entry( name, files ):
 def make_job_details_dict( files, status, markers, active):
 	return { "files":files, "status":status, "markers":markers, "active": active} 
 
+
+################################################################################
+# API
+################################################################################
+
+def allowed_file(filename):
+	""" Helper function that returns true if we can read the provided file type
+	"""
+	allowed_extensions = ['fcs']
+	return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+
+@app.route("/api/1/jobs/<int:job_id>/upload", methods = ['GET','POST'])
+def upload(job_id):
+	""" Provides API for reading/uploading the files on the disk
+	"""
+	base_path = join(app.config['DATA_PATH'], '%09d' % (job_id,))
+
+	# Make the directory for holding the data if necessary
+	if not os.path.exists(base_path):
+		os.makedirs(base_path)
+
+	if request.method == 'POST':
+		file = request.files['file']
+		if file:
+			filename = secure_filename(file.filename)
+			filename = join(base_path, filename)
+			mimetype = file.content_type
+
+			if not allowed_file(filename):
+				result = uploadfile(name=filename, type=mimetype, size=0, not_allowed_msg="Filetype not allowed", job_id = job_id)
+			else:
+				# save file to disk
+				uploaded_file_path = join(base_path, filename)
+				file.save(uploaded_file_path)
+				
+				# get file size after saving
+				size = os.path.getsize(uploaded_file_path)
+
+                # return json for js call back
+				result = uploadfile(name=filename, type=mimetype, size=size, job_id = job_id)
+	
+	if request.method == 'GET':
+		# get all file in ./data directory
+		files = [ f for f in os.listdir(base_path) if os.path.isfile(os.path.join(base_path,f)) and f not in IGNORED_FILES ]    
+		file_display = []
+
+		for f in files:
+			size = os.path.getsize(os.path.join(base_path, f))
+			file_saved = uploadfile(name=f, size=size, job_id = job_id)
+			file_display.append(file_saved.get_file())
+
+		return simplejson.dumps({"files": file_display})
+
+	return redirect(url_for('/'))
+
+@app.route("/api/1/jobs/<int:job_id>/delete/<string:filename>", methods=['DELETE'])
+def delete(job_id, filename):
+	base_path = join(app.config['DATA_PATH'], '%09d' % (job_id,))
+	file_path = os.path.join(base_path, filename)
+    #file_thumb_path = os.path.join(app.config['THUMBNAIL_FOLDER'], filename)
+
+	if os.path.exists(file_path):
+		try:
+			os.remove(file_path)
+			return simplejson.dumps({filename: 'True'})
+		except:
+			return simplejson.dumps({filename: 'False'})
+
+@app.route("/api/1/jobs/<int:job_id>/data/<string:filename>", methods=['GET'])
+def get_file(job_id, filename):
+	base_path = join(app.config['DATA_PATH'], '%09d' % (job_id,))
+	return send_from_directory(base_path, filename=filename)
 
 @app.route("/api/1/jobs/<int:job_id>", methods=["GET"])
 def api_handle_job_details(job_id):
